22f167f6564798a784b9e38dec36799357c2941d
PC-23 <it.pgee.burgas@gmail.com>
2024-05-15 11:55:49 +0300

    zubzzazub

--- Controller.cs
+++ Controller.cs
@@ -25,17 +25,34 @@ public class Controller
         double salary = double.Parse(args[3]);
         string type = args[4];
 
-        JobOffer offer;
-
-        if (type == "onsite")
+        if (!categories.ContainsKey(category))
         {
-            string city = args[5];
-            offer = new OnSiteJobOffer(jobTitle, company, salary, city);
+            return $"Category {category} does not exist!";
         }
-        else
+
+        JobOffer offer;
+        try
         {
-            bool fullyRemote = bool.Parse(args[5]);
-            offer = new RemoteJobOffer(jobTitle, company, salary, fullyRemote);
+            if (type == "onsite")
+            {
+                if (args.Count < 6) return "Invalid number of arguments for onsite job offer!";
+                string city = args[5];
+                offer = new OnSiteJobOffer(jobTitle, company, salary, city);
+            }
+            else if (type == "remote")
+            {
+                if (args.Count < 6) return "Invalid number of arguments for remote job offer!";
+                bool fullyRemote = bool.Parse(args[5]);
+                offer = new RemoteJobOffer(jobTitle, company, salary, fullyRemote);
+            }
+            else
+            {
+                return "Invalid job offer type!";
+            }
+        }
+        catch (ArgumentException ex)
+        {
+            return ex.Message;
         }
 
         categories[category].AddJobOffer(offer);
